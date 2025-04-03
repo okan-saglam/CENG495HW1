d18f48e66125126fa9afa23d299c16a669c3e228
okan-saglam <okan415461@gmail.com>
2025-04-03 23:14:52 +0300

    little fix

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/user.model');
+const mongoose = require('mongoose');
 
 // Register (public endpoint for regular users)
 const register = async (req, res) => {
